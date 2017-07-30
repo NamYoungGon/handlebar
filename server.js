ca944ec13b93566713c614ff7004ca740749fb5c
nami <nyg4628@gmail.com>
2017-07-30 21:26:32 +0900

    init commit

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.set('port', 3000);
+app.disable('x-powered-by');
+
+const handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// MORE IMPORTS HERE
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send('Express Works');
+    res.render('home');
 });
 
-app.listen(app.get('port'), () => {
-    console.log('Express started press Ctrl-C to terminate');
+app.get('/about', (req, res) => {
+    res.render('about');
 });
+
+
+
+app.listen(app.get('port'), () => {
+    console.log('Express started on http://localhost:' + app.get('port') + ' press Ctrl-C to terminate');
+});
+
+
+
+
+
